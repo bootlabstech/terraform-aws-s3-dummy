5b22b3c34992fbeb3844a0a9fbc96111d4304d87
lalitha2407 <lalitha@bootlabstech.com>
2022-12-30 13:23:56 +0530

    provider version changes

--- provider.tf
+++ provider.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = ">=0.13"
-  
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.18.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.26.0"
     }
   }
+
+  required_version = ">= 1.0.11"
+}
+
+provider "aws" {
+  region = var.region
 }
